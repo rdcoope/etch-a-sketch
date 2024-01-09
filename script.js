9865a6213458703dc9d7aaaeeafad1bead122bd2
Richard <rdcoope@gmail.com>
2024-01-08 21:20:53 -0600

    Eliminate children from grid when modifying size

--- script.js
+++ script.js
@@ -1,21 +1,38 @@
 //create the grid
-const count = 100
-
 const grid = document.querySelector('#grid');
+const gridSize = document.querySelector('#gridSize')
 
-
-for (let i = 0; i < count; i++){
-    const blank = document.createElement('div');
-    blank.setAttribute('class', 'blank')
-
-    for(let j = 1; j < count; j++){
-        const horBlank = document.createElement('div')
-        horBlank.setAttribute('class', 'horizontal')
-        
-    blank.appendChild(horBlank)
+function generateGrid(count) {
+    for (let i = 0; i < count; i++){
+        const blank = document.createElement('div');
+        blank.setAttribute('class', 'blank')
+    
+        for(let j = 0; j < count; j++){
+            const horBlank = document.createElement('div')
+            horBlank.setAttribute('class', 'horizontal')
+            
+        blank.appendChild(horBlank)
+        }
+    
+        grid.appendChild(blank);
+    
+        console.log(i)
+        }
+    
     }
+    
 
-    grid.appendChild(blank);
-    console.log(i)
+let count = 16
+generateGrid(count)
 
-}
+
+
+let countGrid
+gridSize.addEventListener('click', () => {
+    count = prompt('How many squares?')
+    grid.innerHTML = ''
+    generateGrid(count)
+})
+//count = countGrid
+console.log('the grid count is: ' + count)
+
